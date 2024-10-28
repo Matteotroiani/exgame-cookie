5991e2596c47513b5a333ec53ad70b8c07a541bb
Matteo Troiani <matteotroiani02@gmail.com>
2024-10-28 12:56:04 +0100

    lezione 5.1

--- frontend/src/routes/teacher/exams/Exams.tsx
+++ frontend/src/routes/teacher/exams/Exams.tsx
@@ -1,3 +1,45 @@
+import Table from '@mui/joy/Table';
+import Stack from '@mui/joy/Stack';
+import Button from '@mui/joy/Button';
+
+
+
 export const Exams: React.FC = () => {
-  return <>Teacher exams</>;
-};
+  return (
+        <Table aria-label="basic table">
+          <thead>
+            <tr>
+              <th style={{ width: '40%' }}>Esame</th>
+              <th>Classe</th>
+              <th>Modifica</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>JavaScript</td>
+              <td>159</td>
+              <td><Stack direction="row" spacing={1}>
+                <Button>Modifica</Button>
+                <Button>Elimina</Button>
+            </Stack></td>
+            </tr>
+            <tr>
+              <td>HTML e CSS</td>
+              <td>237</td>
+              <td><Stack direction="row" spacing={1}>
+                <Button>Modifica</Button>
+                <Button>Elimina</Button>
+            </Stack></td>
+            </tr>
+            <tr>
+              <td>React</td>
+              <td>356</td>
+              <td><Stack direction="row" spacing={1}>
+                <Button>Modifica</Button>
+                <Button>Elimina</Button>
+            </Stack></td>
+            </tr>
+          </tbody>
+        </Table>
+      );
+    }
